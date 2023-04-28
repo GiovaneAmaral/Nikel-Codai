527762459f3589d66be2de1d488c24c9c117e65e
Giovande do Amaral de Oliveira <giovaneamaral161@gmail.com>
2023-04-28 13:54:41 -0300

    Alteracao no Projeto

---
+++ js/base.js
@@ -0,0 +1,96 @@
+const nome = "Giovane";
+let nome2 =  "";
+let pessoaDefault = {
+    nome: "Giovane Amaral",
+    idade: "19",
+    trabalho: "Estudante"
+}
+
+let nomes = ["Giovane Amaral", "Maria Clara", "Leandra Silva"];
+let pessoas = [
+    {
+    nome: "Giovane Amaral",
+    idade: "19",
+    trabalho: "Estudante"
+},
+    {
+        nome: "Maria Clara",
+        idade: "19",
+        trabalho: "Estudante"
+    }    
+];
+
+/*console.log("Vai aparecer minha cosnt")
+console.log(nome2);
+
+nome2 = "Giovane Amaral";
+
+console.log("Valor alterado:");
+console.log(nome2);
+*/
+function alterarNome(){
+    nome2 = "Maria Silva";
+    console.log("Valor alterado");
+    console.log(nome2);
+}
+
+function recebealteraNome(novoNome){
+    nome2 = novoNome;
+    console.log("Valor alterado recebendo um nome");
+    console.log(nome2);
+}
+
+function imprimirPessoa(pessoa){
+    console.log("Nome:");
+console.log(pessoa.nome);
+
+console.log("Idade:");
+console.log(pessoa.idade);
+
+console.log("Trabaçho:");
+console.log(pessoa.trabalho);
+
+}
+
+function adicionarPessoa(pessoa){
+    pessoas.push(pessoa);
+}
+
+function imprimirPessoas() {
+    pessoas.forEach((item) => {
+    console.log("Nome:");
+    console.log(item.nome);
+
+    console.log("Idade:");
+    console.log(item.idade);
+
+    console.log("trabalho:");
+    console.log(item.trabalho);
+    })
+}
+
+
+imprimirPessoas();
+
+adicionarPessoa({
+    nome: "Pedro Silva",
+    idade: "50",
+    trabalho: "Peão"
+});
+
+console.log(pessoas);
+
+/*imprimirPessoa(pessoaDefault);
+
+imprimirPessoa({
+    nome: "Maria",
+    idade: "19",
+    trabalho: "Estudante"
+})*/
+
+
+
+
+//recebealteraNome("João Corre");
+//recebealteraNome("Maria Silva");
+//alterarNome();
